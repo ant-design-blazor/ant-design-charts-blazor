2cb1f7fa0b7bfb042ac5d5166bc01404117f77a4
TimChen <TimChen@live.com>
2020-07-15 19:29:44 +0800

    StackedBar

---
+++ src/AntDesign.Charts/Components/StackedBar/IStackedBarViewConfig.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AntDesign.Charts
+{
+    public interface IStackedBarViewConfig : IBarViewConfig
+    {
+        public string stackField { get; set; }
+    }
+}
---
+++ src/AntDesign.Charts/Components/StackedBar/StackedBar.razor.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AntDesign.Charts
+{
+    public partial class StackedBar<TItem> : ChartComponentBase<TItem>
+    {
+        [Parameter]
+        public StackedBarConfig Config { get; set; }
+        protected override string ChartType { get; set; } = "StackedBar";
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            await base.OnAfterRenderAsync(firstRender);
+
+            if (firstRender)
+            {
+                if (Config == null) Config = new StackedBarConfig();
+                SetIViewConfig(Config);
+                await JS.InvokeVoidAsync(CreateChart, ChartType, Ref, Config, OtherConfig);
+            }
+        }
+    }
+}
