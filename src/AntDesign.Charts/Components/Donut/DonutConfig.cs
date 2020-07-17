18f32e0128876f7aad9350a5e9f3bda9c830fbfb
TimChen <TimChen@live.com>
2020-07-17 15:54:12 +0800

    pie

---
+++ src/AntDesign.Charts/Components/Donut/DonutConfig.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AntDesign.Charts
+{
+    public class DonutConfig : IDonutViewConfig, IPlotConfig
+    {
+        public int? innerRadius { get; set; }
+        public DonutViewConfigStatistic statistic { get; set; }
+        public string angleField { get; set; }
+        public string colorField { get; set; }
+        public decimal? radius { get; set; }
+        public GraphicStyle pieStyle { get; set; }
+        public PieLabelConfig label { get; set; }
+        public string renderer { get; set; }
+        public object data { get; set; }
+        public object meta { get; set; }
+        public string padding { get; set; }
+        public string xField { get; set; }
+        public string yField { get; set; }
+        public string[] color { get; set; }
+        public Axis xAxis { get; set; }
+        public Axis yAxis { get; set; }
+        public Tooltip tooltip { get; set; }
+        public Legend legend { get; set; }
+        public object animation { get; set; }
+        public string theme { get; set; }
+        public object responsiveTheme { get; set; }
+        public Interaction[] interactions { get; set; }
+        public bool? responsive { get; set; }
+        public Title title { get; set; }
+        public Description description { get; set; }
+        public GuideLineConfig[] guideLine { get; set; }
+        public ViewConfigDefaultState defaultState { get; set; }
+        public string name { get; set; }
+        public bool? forceFit { get; set; }
+        public int? width { get; set; }
+        public int? height { get; set; }
+        public int? pixelRatio { get; set; }
+        public bool? localRefresh { get; set; }
+        Label IViewConfig.label { get; set; }
+    }
+
+    public interface IDonutViewConfig : IPieViewConfig
+    {
+        public int? innerRadius { get; set; }
+
+        public DonutViewConfigStatistic statistic { get; set; }
+    }
+
+    public class DonutViewConfigStatistic
+    {
+        public bool? visible { get; set; }
+        /// <summary>
+        ///  指标卡 总计值 标签 
+        /// </summary>
+        public string totalLabel { get; set; }
+        /// <summary>
+        ///  触发显示的事件 
+        /// </summary>
+        public string triggerOn { get; set; }// 'mouseenter'
+        /// <summary>
+        ///  触发隐藏的事件 
+        /// </summary>
+        public string triggerOff { get; set; }//'mouseleave'
+        public object content { get; set; }//OneOf<string, DonutStatisticContent>
+        public string htmlContent { get; set; }//(...args: any) => string
+    }
+}
---
+++ src/AntDesign.Charts/Components/Pie/PieConfig.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AntDesign.Charts
+{
+    public class PieConfig : IPieViewConfig, IPlotConfig
+    {
+        public string angleField { get; set; }
+        public string colorField { get; set; }
+        public decimal? radius { get; set; }
+        public GraphicStyle pieStyle { get; set; }
+        public PieLabelConfig label { get; set; }
+        public string renderer { get; set; }
+        public object data { get; set; }
+        public object meta { get; set; }
+        public string padding { get; set; }
+        public string xField { get; set; }
+        public string yField { get; set; }
+        public string[] color { get; set; }
+        public Axis xAxis { get; set; }
+        public Axis yAxis { get; set; }
+        public Tooltip tooltip { get; set; }
+        public Legend legend { get; set; }
+        public object animation { get; set; }
+        public string theme { get; set; }
+        public object responsiveTheme { get; set; }
+        public Interaction[] interactions { get; set; }
+        public bool? responsive { get; set; }
+        public Title title { get; set; }
+        public Description description { get; set; }
+        public GuideLineConfig[] guideLine { get; set; }
+        public ViewConfigDefaultState defaultState { get; set; }
+        public string name { get; set; }
+        public bool? forceFit { get; set; }
+        public int? width { get; set; }
+        public int? height { get; set; }
+        public int? pixelRatio { get; set; }
+        public bool? localRefresh { get; set; }
+        Label IViewConfig.label { get; set; }
+    }
+
+    public interface IPieViewConfig : IViewConfig
+    {
+        public string angleField { get; set; }
+        public string colorField { get; set; }
+        public decimal? radius { get; set; }
+        public GraphicStyle pieStyle { get; set; }//OneOf<GraphicStyle, ((...args: any[]) => GraphicStyle)>
+        public PieLabelConfig label { get; set; }
+    }
+
+    public interface IPieLabelConfig : ILabel
+    {
+        public bool? visible { get; set; }
+        public object formatter { get; set; }//OneOf<(text: string, int?, undefined,null, item: any, idx: number, ...extras: any[]) => string>
+        /// <summary>
+        ///  whether 
+        /// </summary>
+        public bool? adjustPosition { get; set; }
+        /// <summary>
+        ///  allow label overlap 
+        /// </summary>
+        public bool? allowOverlap { get; set; }
+        public bool? autoRotate { get; set; }
+        public int? labelHeight { get; set; }
+        public int? offset { get; set; }// OneOf<int?, string>
+        public int? offsetX { get; set; }
+        public int? offsetY { get; set; }
+        public PieLabelConfigLine line { get; set; }
+        public TextStyle style { get; set; }
+    }
+
+    public interface PieLabelConfigLine
+    {
+        public bool? visible { get; set; }
+        public bool? smooth { get; set; }
+        public string stroke { get; set; }
+        public int? lineWidth { get; set; }
+    }
+
+    public class PieLabelConfig : IPieLabelConfig
+    {
+        public bool? visible { get; set; }
+        public object formatter { get; set; }
+        public bool? adjustPosition { get; set; }
+        public bool? allowOverlap { get; set; }
+        public bool? autoRotate { get; set; }
+        public int? labelHeight { get; set; }
+        public int? offset { get; set; }
+        public int? offsetX { get; set; }
+        public int? offsetY { get; set; }
+        public PieLabelConfigLine line { get; set; }
+        public TextStyle style { get; set; }
+        public string type { get; set; }
+        public int? precision { get; set; }
+        public string suffix { get; set; }
+        public string position { get; set; }
+        public bool? adjustColor { get; set; }
+        public string field { get; set; }
+    }
+
+}
