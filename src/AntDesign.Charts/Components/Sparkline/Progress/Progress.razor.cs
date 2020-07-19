a850e72491f01b2c8c97a99470b0bc22a15ad558
TimChen <TimChen@live.com>
2020-07-19 20:11:35 +0800

    add Sparkline

---
+++ src/AntDesign.Charts/Components/Sparkline/Progress/Progress.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AntDesign.Charts
+{
+    public partial class Progress<TItem> : ChartComponentBase<TItem, ProgressConfig>
+    {
+        public Progress() : base("Progress")
+        {
+
+        }
+    }
+}
