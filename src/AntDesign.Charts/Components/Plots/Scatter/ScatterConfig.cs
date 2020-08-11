1844b25e3cab2bbebdb50ea574cae3cdc3a3308b
henry.zhang <1002527441@qq.com>
2020-08-11 20:29:04 +0800

    fix: pointstyle opacity change to double, the label in quadrant change to object.

--- src/AntDesign.Charts/Components/Plots/Scatter/ScatterConfig.cs
+++ src/AntDesign.Charts/Components/Plots/Scatter/ScatterConfig.cs
@@ -142,7 +142,7 @@ namespace AntDesign.Charts
         [JsonPropertyName("lineStyle")]
         public object LineStyle { get; set; }
         [JsonPropertyName("label")]
-        public ILabel Label { get; set; }
+        public object Label { get; set; }
     }
 
     public class QuadrantConfig : IQuadrantConfig
@@ -160,7 +160,7 @@ namespace AntDesign.Charts
         [JsonPropertyName("lineStyle")]
         public object LineStyle { get; set; }
         [JsonPropertyName("label")]
-        public ILabel Label { get; set; }
+        public object Label { get; set; }
     }
 
     public interface ITrendlineConfig
