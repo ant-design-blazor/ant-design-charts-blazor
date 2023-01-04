8e7c2c7c8334e404c77757d469d8a38b99264d53
Jerry <superjerry88@hotmail.com>
2023-01-05 05:03:30 +0800

    Minor cleanup

--- src/AntDesign.Charts/Components/Configs/Annotation/Annotation.cs
+++ src/AntDesign.Charts/Components/Configs/Annotation/Annotation.cs
@@ -23,53 +23,68 @@ namespace AntDesign.Charts
         public static string TypeHtml = "html";
         public static string TypeShape = "shape";
 
-        [JsonPropertyName("style")] public IGraphicStyle Style { get; set; }
-        [JsonIgnore] public OneOf<object, object[], string[]> Start { get; set; }
-        [JsonPropertyName("start")] public object StartMapping => Start.Value;
-        [JsonIgnore] public OneOf<object, object[], string[]> End { get; set; }
-        [JsonPropertyName("end")] public object EndMapping => End.Value;
-
+        [JsonPropertyName("style")] 
+        public IGraphicStyle Style { get; set; }
+        [JsonIgnore] 
+        public OneOf<object, object[], string[]> Start { get; set; }
+        [JsonPropertyName("start")] 
+        public object StartMapping => Start.Value;
+        [JsonIgnore] 
+        public OneOf<object, object[], string[]> End { get; set; }
+        [JsonPropertyName("end")] 
+        public object EndMapping => End.Value;
         [Obsolete("X is not recommended. Use Position instead")]
         public int? X { get; set; }
-
         [Obsolete("Y is not recommended. Use Position instead")]
         public int? Y { get; set; }
-
-        [JsonIgnore] public OneOf<object, object[], string[]> Position { get; set; }
-        [JsonPropertyName("position")] public object PositionMapping => Position.Value;
-        [JsonPropertyName("content")] public string Content { get; set; }
-        [JsonPropertyName("rotate")] public int? Rotate { get; set; }
-        [JsonPropertyName("offsetX")] public int? OffsetX { get; set; }
-        [JsonPropertyName("offsetY")] public int? OffsetY { get; set; }
-        [JsonPropertyName("background")] public AnnotationBackgroundCfg Background { get; set; }
-        [JsonPropertyName("maxLength")] public int? MaxLength { get; set; }
-        [JsonPropertyName("autoEllipsis")] public bool? AutoEllipsis { get; set; }
+        [JsonIgnore] 
+        public OneOf<object, object[], string[]> Position { get; set; }
+        [JsonPropertyName("position")] 
+        public object PositionMapping => Position.Value;
+        [JsonPropertyName("content")] 
+        public string Content { get; set; }
+        [JsonPropertyName("rotate")] 
+        public int? Rotate { get; set; }
+        [JsonPropertyName("offsetX")] 
+        public int? OffsetX { get; set; }
+        [JsonPropertyName("offsetY")] 
+        public int? OffsetY { get; set; }
+        [JsonPropertyName("background")] 
+        public AnnotationBackgroundCfg Background { get; set; }
+        [JsonPropertyName("maxLength")] 
+        public int? MaxLength { get; set; }
+        [JsonPropertyName("autoEllipsis")] 
+        public bool? AutoEllipsis { get; set; }
 
         [JsonPropertyName("ellipsisPosition")]
         public object EllipsisPosition { get; set; } //todo: AntDesign documentation lacking example
 
-        [JsonPropertyName("isVertical")] public bool? IsVertical { get; set; }
-        [JsonPropertyName("text")] public AnnotationTextCfg Text { get; set; }
-        [JsonPropertyName("top")] public bool? Top { get; set; }
-        [JsonPropertyName("animate")] public bool? Animate { get; set; }
-        [JsonPropertyName("animateOption")] public IAnimation AnimateOption { get; set; }
-        [JsonPropertyName("src")] public string Src { get; set; }
-        [JsonPropertyName("autoAdjust")] public bool? AutoAdjust { get; set; }
-        [JsonPropertyName("point")] public DataMarkerPointCfg Point { get; set; }
-        [JsonPropertyName("line")] public DataMarkerLineCfg Line { get; set; }
-
+        [JsonPropertyName("isVertical")] 
+        public bool? IsVertical { get; set; }
+        [JsonPropertyName("text")] 
+        public AnnotationTextCfg Text { get; set; }
+        [JsonPropertyName("top")] 
+        public bool? Top { get; set; }
+        [JsonPropertyName("animate")] 
+        public bool? Animate { get; set; }
+        [JsonPropertyName("animateOption")] 
+        public IAnimation AnimateOption { get; set; }
+        [JsonPropertyName("src")] 
+        public string Src { get; set; }
+        [JsonPropertyName("autoAdjust")] 
+        public bool? AutoAdjust { get; set; }
+        [JsonPropertyName("point")] 
+        public DataMarkerPointCfg Point { get; set; }
+        [JsonPropertyName("line")] 
+        public DataMarkerLineCfg Line { get; set; }
         [JsonPropertyName("direction")]
-        public string Direction { get; set; } //todo need checking, incomplete antdesign documentation
-
+        public string Direction { get; set; } //todo incomplete/missing example
         [JsonIgnore] public OneOf<object, string> Html { get; set; } //todo incomplete/missing example
         [JsonPropertyName("htmlContent")] public object HtmlMapping => Html.Value;
-
         [JsonIgnore] public OneOf<object, string> Container { get; set; } //todo incomplete/missing example
         [JsonPropertyName("container")] public object ContainerMapping => Container.Value;
-
-        [JsonIgnore] public OneOf<object, string> Render { get; set; } //todo incomplete/missing examplen
+        [JsonIgnore] public OneOf<object, string> Render { get; set; } //todo incomplete/missing example
         [JsonPropertyName("render")] public object RenderMapping => Render.Value;
-
         [JsonPropertyName("lineLength")] public int? LineLength { get; set; }
         [JsonPropertyName("region")] public IStateConfig Region { get; set; }
         [JsonPropertyName("color")] public string Color { get; set; }
